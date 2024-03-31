ebe69f7bed46f2df4d89d779aeb4bd539d068e80
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-03-31 14:26:08 +0530

    Update arr_fns.js

--- Javascript/arr_fns.js
+++ Javascript/arr_fns.js
@@ -7,3 +7,9 @@ console.log(sqr_nums)
 const nums2 = [1, 2, 3, 4, 5];
 const even_nums = nums2.filter((item) => item % 2 == 0);
 console.log(even_nums)
+
+// find - 
+const fruits = ["apple", "mango", "watermelon", "orange"];
+const target = "mango"
+const find_fruit = fruits.find(fruit => fruit === target);
+console.log(find_fruit)
