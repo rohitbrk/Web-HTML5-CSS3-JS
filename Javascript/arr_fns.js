4ef9fcd2eb964f6566f7502fdff93cc890178e24
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-03-31 14:22:35 +0530

    Create arr_fns.js

---
+++ Javascript/arr_fns.js
@@ -0,0 +1,9 @@
+// map - 
+const nums = [1, 2, 3, 4, 5];
+const sqr_nums = nums.map((item) => item * 2);
+console.log(sqr_nums)
+
+// filter - 
+const nums2 = [1, 2, 3, 4, 5];
+const even_nums = nums2.filter((item) => item % 2 == 0);
+console.log(even_nums)
