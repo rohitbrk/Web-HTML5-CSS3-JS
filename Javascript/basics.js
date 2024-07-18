de31db6016ed8f20d56898444ad0278080afb87f
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-07-18 10:33:47 +0530

    Update basics.js

--- Javascript/basics.js
+++ Javascript/basics.js
@@ -1,4 +1,18 @@
-// Javascript (Programming language) is used to add functionality to webpage such as alerts, form-submissions etc.
+// Javascript (Programming language) is used to add functionality to webpage such as alerts, form-submissions, dynamic behaviour etc.
+
+// variables are used to store the value of elements. we can declare variables in three ways
+// var - more traditional way (better to avoid this)
+// let - modern way - ES5 way - used to declare variables whose can be altered afterwards
+// const - modern way - ES5 way - used to declare variables whose cannot be altered afterwards
+var a = 1
+let b = 2
+const c = 3
+
+// operations and operators
+let result1 = a + b // addition
+let result2 = a - b // substraction
+let result3 = a * b // multiplication
+let result4 = a / b // division
 
 function greet(){
   window.alert("Hey")
